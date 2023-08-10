ece7b65349d19b0ced58dc6363d2231b5508ff8e
Lilserchil <welly.serchil@gmail.com>
2023-08-10 13:06:01 -0700

    changes for 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
-/** 
- *testing usage of puts() function
+/**
+ * testing usage of puts() function
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/* return 0 for a successful program execution */
+        /* return 0 for a successful program execution */
 	return (0);
 }
