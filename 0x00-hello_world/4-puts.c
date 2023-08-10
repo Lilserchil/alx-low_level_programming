8eb61f57b560ca9df0eb9c1fa5df260e39accf5b
Lilserchil <welly.serchil@gmail.com>
2023-08-10 13:39:46 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-/**
- * testing usage of puts() function
- */
-int main(void)
-{
+1#include <stdio.h>
+2/**
+3 * testing usage of puts() function
+4 */
+5 int main(void)
+{6
 	puts("\"Programming is like building a multilingual puzzle");
         /* return 0 for a successful program execution */
 	return (0);
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * Using printf in thr main function
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
