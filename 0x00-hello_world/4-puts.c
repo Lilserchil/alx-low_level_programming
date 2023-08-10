4c76950c9bd523c38e3b31828d00e1fb368b2a84
Lilserchil <welly.serchil@gmail.com>
2023-08-10 11:52:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #!/bin/bash
 #include <stdio.h>
-
+/**
+ * using puts() fxn in the main function
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
