9b09fcc98bc6207f9c377a4d76851170df150061
Lilserchil <welly.serchil@gmail.com>
2023-08-10 23:06:00 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,19 @@
-#include<stdio.h>
-/**main-Entry point 
- *  
- *Return:Always 0 (Success)
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * main - Prints a string with printf
+ *
+ * Return: 0 if program executed successfully
  */
 int main(void)
 {
-        fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-        return (1);
+	int stdErrFileNo = (2);
+	char buf[60];
+	size_t nBytes;
+
+	strcpy(buf, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	nBytes = strlen(buf);
+	write(stdErrFileNo, buf, nBytes);
+	return (1);
 }
