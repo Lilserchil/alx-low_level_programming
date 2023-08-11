25d5930366b5043b15f679296fd1d9e01c6b9c71
Lilserchil <welly.serchil@gmail.com>
2023-08-11 03:43:10 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype,h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+int lowerCase = 'a';
+while (lowerCase <= 'z')
+{
+putchar(lowerCase);
+lowercase += 1;
+}
+putchar("\n");
+return (0);
+}
