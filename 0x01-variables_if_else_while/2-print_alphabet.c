968acc91c6bdfd9a5d973a9c17855182cfc30dbc
Lilserchil <welly.serchil@gmail.com>
2023-08-11 03:50:00 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,13 @@
  */
 int main(void)
 {
-int lowerCase = 'a';
-while (lowerCase <= 'z')
-{
-putchar(lowerCase);
-lowercase += 1;
-}
-putchar("\n");
-return (0);
+	int lowerCase = 'a';
+	while (lowerCase <= 'z')
+	{
+		putchar (lowerCase);
+		lowerCase += 1;
+
+	}
+	putchar("\n")
+		return (0);
 }
