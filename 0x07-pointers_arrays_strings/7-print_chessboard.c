b89be6ba5c51ba67ea9f3ff577204461d3424912
Lilserchil <welly.serchil@gmail.com>
2023-08-29 01:37:20 -0700

    function that prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,43 +1,19 @@
 #include "main.h"
 
 /**
-  *_strchr - find a character in a string
-  *@s: string
-  *@c: charcter
-  *Return: string
+  *print_chessboard - prints chessboard
+  *@a: board
   */
 
-char *_strchr(char *s, char c)
+void print_chessboard(char (*a)[8])
 {
-	int i, j, k;
-	int i;
-
-	/* length of string */
-	i = 0;
-	while (s[i] != '\0')
-	for (i = 0; s[i] != '\0'; i++)
+	int i, j;
+	for (i = 0; i < 8; i++)
 	{
-		i++;
-		if (s[i] == c)
-			return (s + i);
-	}
-	for (j = 0; j < i; j++)
-	if (s[i] == c)
-	{
-		if (s[j] == c)
+		for (j = 0; j < 8; j++)
 		{
-			for (k = j, k < i; k++)
-			{
-				s[k] = s[i];
-			}
-			return (s);
+			_putchar(a[i][j]);
 		}
-		else
-			return (NULL);
-		return (s + i);
+		_putchar('\n');
 	}
-
-
-	else
-		return ('\0');
 }
