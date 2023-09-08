78665564021634225d7fe3b669f695b45550c6f1
Lilserchil <welly.serchil@gmail.com>
2023-09-08 11:15:12 -0700

    changes to 1-string_nconcat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,21 +16,18 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	for (i = 0; s1[i] != '\0'; i++)
 		s1len++;
 	for (i = 0; s2[i] != '\0'; i++)
 		s2len++;
-
 	output = malloc(sizeof(char) * (s1len + s2len) + 1);
-
 	if (output == NULL)
 		return (NULL);
-
 	for (i = 0; s1[i] != '\0'; i++)
 		output[i] = s1[i];
 	for (i = 0; s2[i] != '\0'; i++)
 		output[s1len + i] = s1[i];
+		output[s1len + i] = s2[i];
 
 	return (output);
 }
